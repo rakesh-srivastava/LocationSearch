7156c1deeb2c46fcc1f9de2a20d7a0b893f4540a
rakeshsrivastava <rakeshsrivastava@gmail.com>
2022-06-02 08:07:39 +0000

    Renamed namespace

--- LocSearchUnitTest/LocationSearchUnitTest.cs
+++ LocSearchUnitTest/LocationSearchUnitTest.cs
@@ -6,7 +6,7 @@ using ProjLocationSearch.Controllers;
 using ProjLocationSearch.Models;
 using Xunit.Sdk;
 
-namespace LocSearchUnitTest
+namespace ProjLocationSearchUnitTest
 {
     [TestClass]
     public class LocationSearchUnitTest
@@ -19,13 +19,13 @@ namespace LocSearchUnitTest
             Location pLocation = new Location(double.Parse("51.644054"), double.Parse("5.6548692"),"testname");
             int maxDistance = 100;
             int maxResults = 10;
-            TaskLocationSearch sr = new TaskLocationSearch();
-            TaskLocationSearch actual;
+            TaskLocationSearch taskLocationSearch = new TaskLocationSearch();
+            TaskLocationSearch actualLocation;
             //Act
-            sr.StartSearch("52.2165425", "5.4778534", maxDistance.ToString(), maxResults.ToString());
-            actual = sv.filterLocation(pLocation, maxDistance, maxResults, sr);
-            //Assert
-            Assert.AreEqual(maxResults.ToString(), actual.maxResults);
+            taskLocationSearch.StartSearch("52.2165425", "5.4778534", maxDistance.ToString(), maxResults.ToString());
+            actualLocation = sv.filterLocation(pLocation, maxDistance, maxResults, taskLocationSearch);
+            
+            Assert.AreEqual(maxResults.ToString(), actualLocation.maxResults);
         }
     }
 }
